153f4397c68928084ab52622999de6beb8b06da5
Dan Bucholtz <dan.bucholtz@gmail.com>
2018-03-09 23:15:12 -0600

    fix comment

--- go/helpers.go
+++ go/helpers.go
@@ -4,6 +4,7 @@ import (
 	"regexp"
 )
 
+// IsValidUnixCommand determines whether a string is a valid-ish unix command name. Note, this does not actually validate that it's an executable command, or installed on ones system, etc
 func IsValidUnixCommand(s string) bool {
 	match, _ := regexp.MatchString("^[a-zA-Z0-9_+-]+$", s)
 	return match
