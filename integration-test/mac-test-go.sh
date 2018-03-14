6467b3931678792fc28f861934232891ae6bea59
Dan Bucholtz <dan.bucholtz@gmail.com>
2018-03-13 20:35:07 -0500

    test(integration): added integration tests

---
+++ integration-test/mac-test-go.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+./go/go &
+serverPid=$!
+echo "Starting up the Go implementation of the TCP server with PID $serverPid"
+sleep 5
+echo "Starting Mac (darwin) test harness"
+./integration-test/mac-distro -concurrency 100 &> ./integration-test/go-server-integration-results.txt
+blah=$(cat ./integration-test/go-server-integration-results.txt | grep "All tests passed")
+retVal=$?
+if [ ! $? -eq 0 ]; then
+    echo "The Integration Tests Failed"
+fi
+echo "Shutting down server"
+kill $serverPid
+echo "Deleting tmp files"
+# rm -rf ./integration-test/go-server-integration-results.txt
+echo "Exiting with return code $retVal"
+exit $retVal
+
